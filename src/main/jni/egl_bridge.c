97443701eca7489b0da00f824738aad49a9e678a
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2020-12-29 14:31:49 +0700

    Compile fix (6/?)

--- src/main/jni/egl_bridge.c
+++ src/main/jni/egl_bridge.c
@@ -65,7 +65,7 @@ JNIEXPORT jboolean JNICALL Java_org_lwjgl_glfw_GLFW_nativeEglSwapBuffers(JNIEnv 
 
 JNIEXPORT jboolean JNICALL Java_org_lwjgl_glfw_GLFW_nativeEglSwapInterval(JNIEnv *env, jclass clazz, jint interval) {
 	// return eglSwapInterval(potatoBridge.eglDisplay, interval);
-	setSwapInterval(CURR_GL_CONTEXT. interval);
+	// setSwapInterval(CURR_GL_CONTEXT. interval);
 	return JNI_FALSE;
 }
 
