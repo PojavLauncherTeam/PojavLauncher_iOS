1f7bc72ce2bfd646c5ef53fb31bf8148b3b528e0
Duy Tran Khanh <khanhduytran0@users.noreply.github.com>
2021-01-01 12:01:16 +0700

    Add -XstartOnFirstThread and -Djava.library.path

--- Natives/JavaLauncher.c
+++ Natives/JavaLauncher.c
@@ -66,6 +66,8 @@ int launchJVM(int argc, char *argv[]) {
     // Check if JVM restarts
     if (!started) {
         margv[margc++] = "/usr/lib/jvm/java-16-openjdk/bin/java";
+        margv[margc++] = "-XstartOnFirstThread";
+        margv[margc++] = "Djava.library.path=/Applications/PojavLauncher.app/Frameworks";
         debug("Reading custom JVM args (overrideargs.txt)");
         char jvmargs[10000];
         FILE* argsFile = fopen(args_path, "r");
