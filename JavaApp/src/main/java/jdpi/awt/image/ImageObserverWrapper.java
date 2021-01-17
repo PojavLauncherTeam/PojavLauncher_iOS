79db8bca998384bdee803628d57054840c5453a2
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-01-17 15:54:25 +0700

    Update ImageObserverWrapper.java

--- JavaApp/src/main/java/jdpi/awt/image/ImageObserverWrapper.java
+++ JavaApp/src/main/java/jdpi/awt/image/ImageObserverWrapper.java
@@ -2,9 +2,10 @@ package jdpi.awt.image;
 
 import jdpi.awt.Image;
 
-public void ImageObserverWrapper {
-    public java.awt.image.ImageObserver wrap(final ImageObserver observer) {
+public class ImageObserverWrapper {
+    public static java.awt.image.ImageObserver wrap(final ImageObserver observer) {
         return new java.awt.image.ImageObserver() {
+            @Override
             public boolean imageUpdate(java.awt.Image img, int infoflags, int x, int y, int width, int height) {
                 if (img instanceof BufferedImage) {
                     observer.imageUpdate((BufferedImage) imf, infoflags, x, y, width, height);
