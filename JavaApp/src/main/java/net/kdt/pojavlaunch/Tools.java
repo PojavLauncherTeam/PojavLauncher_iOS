6059ff3c1e9a7dabe26b9069032f92101761ed3d
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-01-17 19:33:06 +0700

    Uppercase

--- JavaApp/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ JavaApp/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -85,7 +85,7 @@ public final class Tools
         
         new Thread(() -> { try {
         
-        URLClassLoader loader = new URLClassLoader(urlList.toArray(new URL[0]), ClassLoader.getSystemClassloader());
+        URLClassLoader loader = new URLClassLoader(urlList.toArray(new URL[0]), ClassLoader.getSystemClassLoader());
         Class<?> clazz = loader.loadClass(versionInfo.mainClass);
         Method method = clazz.getMethod("main", String[].class);
         method.invoke(null, new Object[]{launchArgs});
