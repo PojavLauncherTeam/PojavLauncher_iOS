53ea80fa0a22f8f0d15e9a456f20dc25182ec4a9
Duy Tran Khanh <khanhduytran0@users.noreply.github.com>
2021-01-30 05:26:39 +0700

    Tiny compile fix

--- Natives/ios_uikit_bridge.m
+++ Natives/ios_uikit_bridge.m
@@ -7,7 +7,7 @@ void UIKit_runOnUIThread(jobject callback) {
     dispatch_async(dispatch_get_main_queue(), ^{
         JNIEnv *env;
         (*runtimeJavaVMPtr)->GetEnv(runtimeJavaVMPtr, &env, JNI_VERSION_1_4);
-        jclass clazz = (*env)->GetObjectClass(env, object);
+        jclass clazz = (*env)->GetObjectClass(env, callback);
         jmethodID method = (*env)->GetStaticMethodID(env, clazz, "onCallback", "()V");
         (*env)->CallStaticVoidMethod(
             env, callback, method
