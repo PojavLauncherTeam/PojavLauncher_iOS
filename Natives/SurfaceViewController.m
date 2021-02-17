f2ab2d48256d45d11494bdab00ec96b67103af0a
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-02-17 14:34:01 +0700

    Set preferredFramesPerSecond to 1000

--- Natives/SurfaceViewController.m
+++ Natives/SurfaceViewController.m
@@ -133,6 +133,7 @@ BOOL shouldTriggerClick = NO;
     MGLKView *view = glView = (MGLKView *) self.view;
     view.drawableDepthFormat = MGLDrawableDepthFormat24;
     view.enableSetNeedsDisplay = YES;
+    view.preferredFramesPerSecond = 1000;
 
     // Init GLES
     self.context = [[MGLContext alloc] initWithAPI:kMGLRenderingAPIOpenGLES3];
