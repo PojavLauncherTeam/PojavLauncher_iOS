a63256f2e3fa1183ab094f803cabccf20a94b4dd
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-04-07 05:32:28 +0700

    Fix libOSMesaOverride (fixes #122)

--- build_natives.sh
+++ build_natives.sh
@@ -8,7 +8,7 @@ mkdir -p build
 cd build
 wget https://github.com/leetal/ios-cmake/raw/master/ios.toolchain.cmake
 cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=ios.toolchain.cmake -DPLATFORM=OS64 -DENABLE_BITCODE=FALSE -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED="NO" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY=""
-cmake --build . --config Release --target pojavexec PojavLauncher
+cmake --build . --config Release --target pojavexec OSMesaOverride PojavLauncher
 cd ../..
 
 # Compile storyboard
