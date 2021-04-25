7409666470434aa62442400dc9894833396d1c1c
Adam Tunnic <atunnic@outlook.com>
2021-04-25 15:05:51 -0400

    Fix workflow

--- scripts/actions_setup.sh
+++ scripts/actions_setup.sh
@@ -3,8 +3,8 @@
 git clone https://github.com/xybp888/iOS-SDKs.git
 mv iOS-SDKs/iPhoneOS13.7.sdk iPhoneOS13.7.sdk
 
-sed -i "7s/-isysroot \/Applications\/Xcode.app\/Contents\/Developer\/Platforms\/iPhoneOS.platform\/Developer\/SDKs\/iPhoneOS13.7.sdk/-isysroot ..\/iPhoneOS13.7.sdk/" build_natives_clang.sh
+sed -i "7s/-isysroot \/Applications\/Xcode.app\/Contents\/Developer\/Platforms\/iPhoneOS.platform\/Developer\/SDKs\/iPhoneOS13.7.sdk/-isysroot ..\/iPhoneOS13.7.sdk/" scripts/build_natives_clang.sh
 
-chmod +x *.sh
+chmod +x scripts/*.sh
 
 echo "Ready to go!"
