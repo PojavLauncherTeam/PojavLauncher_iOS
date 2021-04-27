53b64863f8634e3882ad5d9b1b193aedeb3ea19a
Adam Tunnic <atunnic@outlook.com>
2021-04-27 08:50:39 -0400

    Back to a working build

--- Natives/LoginViewController.m
+++ Natives/LoginViewController.m
@@ -181,7 +181,6 @@ void loginAccountInput(UINavigationController *controller, int type, const char*
 }
 
 - (void)loginMicrosoft {
-    @property (strong) ASWebAuthenticationSession *authVC;
     NSURL *url = [NSURL URLWithString:@"https://login.live.com/oauth20_authorize.srf?client_id=00000000402b5328&response_type=code&scope=service%3A%3Auser.auth.xboxlive.com%3A%3AMBI_SSL&redirect_url=https%3A%2F%2Flogin.live.com%2Foauth20_desktop.srf"];
 
     ASWebAuthenticationSession *authVC =
