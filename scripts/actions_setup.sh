b2d5d8339a1a438a4a30569685e628b0eea5e495
Adam Tunnic <atunnic@outlook.com>
2021-04-26 21:31:03 -0400

    Build with 12.1.2 SDK

--- scripts/actions_setup.sh
+++ scripts/actions_setup.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
 git clone https://github.com/xybp888/iOS-SDKs.git
-mv iOS-SDKs/iPhoneOS13.4.sdk iPhoneOS13.4.sdk
+mv iOS-SDKs/iPhoneOS12.1.2sdk iPhoneOS12.1.2.sdk
 
-sed -i "" "7s/-isysroot \/Applications\/Xcode.app\/Contents\/Developer\/Platforms\/iPhoneOS.platform\/Developer\/SDKs\/iPhoneOS13.7.sdk/-isysroot ..\/iPhoneOS13.4.sdk/" scripts/build_natives_clang.sh
+sed -i "" "7s/-isysroot \/Applications\/Xcode.app\/Contents\/Developer\/Platforms\/iPhoneOS.platform\/Developer\/SDKs\/iPhoneOS13.7.sdk/-isysroot ..\/iPhoneOS12.1.2.sdk/" scripts/build_natives_clang.sh
 
 chmod +x scripts/*.sh
 
