ec9e2c98f75f7de6671c7162fa3ae55eedae14b7
Adam Tunnic <tunnic.adam@gmail.com>
2021-05-10 21:04:02 -0400

    Fix libOSMesaOverride

--- scripts/build_natives_cmake.sh
+++ scripts/build_natives_cmake.sh
@@ -8,10 +8,10 @@ mkdir -p build
 cd build
 wget https://github.com/leetal/ios-cmake/raw/master/ios.toolchain.cmake
 cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=ios.toolchain.cmake -DPLATFORM=OS64 -DENABLE_BITCODE=FALSE -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED="NO" -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY=""
-cmake --build . --config Release --target pojavexec PojavLauncher
+cmake --build . --config Release --target pojavexec OSMesaOverride PojavLauncher
 cd ../..
 
 # Copy to target app
 mkdir -p Natives/build/Release-iphoneos/PojavLauncher.app/Frameworks
-cp Natives/build/Release-iphoneos/libpojavexec.dylib Natives/build/Release-iphoneos/PojavLauncher.app/Frameworks/
+cp Natives/build/Release-iphoneos/*.dylib Natives/build/Release-iphoneos/PojavLauncher.app/Frameworks/
 cp -R Natives/resources/* Natives/build/Release-iphoneos/PojavLauncher.app/
