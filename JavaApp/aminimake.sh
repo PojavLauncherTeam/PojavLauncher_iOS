a93bc7028e192d658209a05149b7ce35d1f7d134
Duy Tran Khanh <khanhduytran0@users.noreply.github.com>
2021-06-02 06:29:00 +0700

    Added local java compiler build script

---
+++ JavaApp/aminimake.sh
@@ -0,0 +1,20 @@
+# This one is only used for local compile on developer iphone, do not execute if
+# - You don't know what will it does.
+# - No OpenJDK installed.
+
+/bin/bash <<'EOF'
+
+set -e
+shopt -s globstar
+
+mkdir -p local_out/classes
+javac -cp "libs/*" -d local_out/classes src/main/java/**/*.java
+
+cd local_out/classes
+jar -c -f ../launcher.jar *
+
+cp ../launcher.jar /Applications/PojavLauncher.app/libs/launcher.jar
+
+echo "BUILD SUCCESSFUL"
+
+EOF
