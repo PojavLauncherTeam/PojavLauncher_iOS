f76c91f1a9d6afefcf253b0349d8fdd9dd25aec8
Duy Tran Khanh <khanhduytran0@users.noreply.github.com>
2021-07-07 07:55:41 +0700

    Get Minecraft 1.17.1 to work

---
+++ JavaApp/src/main/java/org/lwjgl/glfw/GLFWNativeCocoa.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ * MACHINE GENERATED FILE, DO NOT EDIT
+ */
+package org.lwjgl.glfw;
+
+import org.lwjgl.system.*;
+
+import static org.lwjgl.system.APIUtil.*;
+import static org.lwjgl.system.Checks.*;
+import static org.lwjgl.system.JNI.*;
+
+/** Native bindings to the GLFW library's Cocoa native access functions. */
+public class GLFWNativeCocoa {
+
+    /** Contains the function pointers loaded from {@code GLFW.getLibrary()}. */
+    public static final class Functions {
+
+        private Functions() {}
+
+        /** Function address. */
+/*
+        public static final long
+            GetCocoaMonitor = apiGetFunctionAddress(GLFW.getLibrary(), "glfwGetCocoaMonitor"),
+            GetCocoaWindow  = apiGetFunctionAddress(GLFW.getLibrary(), "glfwGetCocoaWindow");
+*/
+
+    }
+
+    protected GLFWNativeCocoa() {
+        throw new UnsupportedOperationException();
+    }
+
+    // --- [ glfwGetCocoaMonitor ] ---
+
+    /**
+     * Returns the {@code CGDirectDisplayID} of the specified monitor.
+     * 
+     * <p>Note: This function may be called from any thread. Access is not synchronized.</p>
+     *
+     * @param monitor the GLFW monitor
+     *
+     * @return The {@code CGDirectDisplayID} of the specified monitor, or {@code kCGNullDirectDisplay} if an error occurred.
+     *
+     * @since version 3.1
+     */
+    @NativeType("CGDirectDisplayID")
+    public static int glfwGetCocoaMonitor(@NativeType("GLFWmonitor *") long monitor) {
+/*
+        long __functionAddress = Functions.GetCocoaMonitor;
+        if (CHECKS) {
+            check(monitor);
+        }
+        return invokePI(monitor, __functionAddress);
+*/
+
+        return 1;
+    }
+
+    // --- [ glfwGetCocoaWindow ] ---
+
+    /**
+     * Returns the {@code NSWindow} of the specified GLFW window.
+     * 
+     * <p>Note: This function may be called from any thread. Access is not synchronized.</p>
+     *
+     * @param window the GLFW window
+     *
+     * @return The {@code NSWindow} of the specified window, or nil if an error occurred.
+     *
+     * @since version 3.0
+     */
+    @NativeType("id")
+    public static long glfwGetCocoaWindow(@NativeType("GLFWwindow *") long window) {
+/*
+        long __functionAddress = Functions.GetCocoaWindow;
+        if (CHECKS) {
+            check(window);
+        }
+        return invokePP(window, __functionAddress);
+*/
+
+        // return NULL instead to skip calling to NSWindow stuff, accroding to decompiling MC source code
+        return 0l;
+    }
+
+}
