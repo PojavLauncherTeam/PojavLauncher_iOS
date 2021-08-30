1c91691d9690af6a174a52b0e00c58b309d2f8dd
Duy Tran Khanh <khanhduytran0@users.noreply.github.com>
2021-08-30 14:36:12 +0700

    Workaround for Forge 1.12.2 crashing

--- JavaApp/src/main/java/org/lwjgl/glfw/GLFW.java
+++ JavaApp/src/main/java/org/lwjgl/glfw/GLFW.java
@@ -1056,10 +1056,14 @@ public class GLFW
     }
     
     public static void glfwSetWindowSize(long window, int width, int height) {
-        internalGetWindow(window).width = width;
-        internalGetWindow(window).height = height;
-        
-        System.out.println("GLFW: Set size for window " + window + ", width=" + width + ", height=" + height);
+    	try {
+        	internalGetWindow(window).width = width;
+        	internalGetWindow(window).height = height;
+        	System.out.println("GLFW: Set size for window " + window + ", width=" + width + ", height=" + height);
+        } catch (Throwable th) {
+        	System.err.println("Unable to set window size for window " + window);
+        	th.printStackTrace();
+        }
     }
     
 	public static void glfwShowWindow(long window) {
