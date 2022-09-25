87242860c616295f8f9700e0c33bdecf1a073db5
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2022-09-25 18:50:55 +0700

    Set minimum RAM allocation slider to 250MB

--- Natives/LauncherPreferencesViewController2.m
+++ Natives/LauncherPreferencesViewController2.m
@@ -199,7 +199,7 @@ typedef void(^CreateView)(UITableViewCell *, NSString *, NSDictionary *);
                 @"hasDetail": @YES,
                 @"icon": @"memorychip",
                 @"type": self.typeSlider,
-                @"min": @((NSProcessInfo.processInfo.physicalMemory / 1048576) * 0.25),
+                @"min": @(250),
                 @"max": @((NSProcessInfo.processInfo.physicalMemory / 1048576) * 0.85),
                 @"enableCondition": ^BOOL(){
                     return ![getPreference(@"auto_ram") boolValue] && whenNotInGame();
