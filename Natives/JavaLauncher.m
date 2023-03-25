PojavLauncherTeam/PojavLauncher_iOS

commit 8d4a22325703647743dc1c0fbd619340b2c3686b

Add switch for customizing AVAudioSession (untested)

Natives/JavaLauncher.m
@@ -285,7 +285,20 @@ int launchJVM(NSString *username, id launchTarget, int width, int height, int mi
     
     // TODO: Add preference toggle 
     NSError *sessionError = nil;
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback withOptions:AVAudioSessionCategoryOptionMixWithOthers error:&sessionError];
+    AVAudioSessionCategory category;
+    AVAudioSessionCategoryOptions options;
+    if([getPreference(@"silence_other_audio") boolValue]) {
+        category = AVAudioSessionCategorySoloAmbient;
+    } else {
+        category = AVAudioSessionCategoryPlayback;
+    }
+    
+    if([getPreference(@"silence_with_switch") boolValue]) {
+        options = AVAudioSessionCategoryOptionMixWithOthers;
+    } else {
+        options = 0;
+    }
+    [[AVAudioSession sharedInstance] setCategory:category withOptions:options error:&sessionError];
     [[AVAudioSession sharedInstance] setActive:YES error:&sessionError];
     
 

Natives/LauncherPreferences.m
@@ -145,6 +145,8 @@ void loadPreferences(BOOL reset) {
     setDefaultValueForPref(envPrefDict, @"renderer", @"auto");
     setDefaultValueForPref(envPrefDict, @"fullscreen_airplay", @YES);
     setDefaultValueForPref(envPrefDict, @"hardware_hide", @NO);
+    setDefaultValueForPref(envPrefDict, @"silence_other_audio", @NO);
+    setDefaultValueForPref(envPrefDict, @"silence_with_switch", @NO);
     setDefaultValueForPref(prefDict, @"slimmed", @NO);
     fillDefaultWarningDict();
     setDefaultValueForPref(prefDict, @"force_unsupported_launch", @NO);

Natives/LauncherPreferencesViewController.m
@@ -236,7 +236,19 @@ typedef void(^CreateView)(UITableViewCell *, NSString *, NSDictionary *);
                       [self.presentingViewController performSelector:@selector(switchToInternalDisplay)];
                   }
               }
-            }
+            },
+            @{@"key": @"silence_other_audio",
+              @"hasDetail": @YES,
+              @"icon": @"speaker.slash",
+              @"type": self.typeSwitch,
+              @"enableCondition": whenNotInGame
+            },
+            @{@"key": @"silence_with_switch",
+              @"hasDetail": @YES,
+              @"icon": @"speaker.zzz",
+              @"type": self.typeSwitch,
+              @"enableCondition": whenNotInGame
+            },
         ], @[
             // Control settings
             @{@"icon": @"gamecontroller"},

Natives/en.lproj/Localizable.strings
@@ -122,7 +122,7 @@
 "Settings" = "Settings";
 
 "preference.section.general" = "General Settings";
-"preference.section.video" = "Video Settings";
+"preference.section.video" = "Video and Audio Settings";
 "preference.section.footer.video" = "Auto renderer allows Minecraft to decide which renderer to use. Decreasing resolution reduces GPU workload for better performance.";
 "preference.section.control" = "Control customization";
 "preference.section.debug" = "UI Debugging settings";
@@ -180,6 +180,10 @@
 
 "preference.title.fullscreen_airplay" = "Fullscreen AirPlay";
 "preference.detail.fullscreen_airplay" = "Allows presenting fullscreen game window on the external display rather than mirroring everything on screen.";
+"preference.title.silence_other_audio" = "Pause audio when game opens";
+"preference.detail.silence_other_audio" = "Enable to allow PojavLauncher to pause audio from other apps, such as Music, when launching the game.";
+"preference.title.silence_with_switch" = "Disable audio with silent switch";
+"preference.detail.silence_with_switch" = "Enable to disable audio when Silent Mode is on. Headphones and external speakers are not affected.";
 "preference.title.default_gamepad_ctrl" = "Controller configuration";
 "preference.title.hardware_hide" = "Hide controls when hardware is connected";
 "preference.detail.hardware_hide" = "Enable the behaviour of automatically disabling the on-screen control overlay when a mouse or game controller is connected.";

commit 7668f3cb9ae298007e2652e63c26344ba81cb654

Fix: audio output issues due to misplaced pref key
Issue introduced in 8d4a22325703647743dc1c0fbd619340b2c3686b

Natives/JavaLauncher.m
@@ -278,17 +278,15 @@ int launchJVM(NSString *username, id launchTarget, int width, int height, int mi
     }
     margv[++margc] = [NSString stringWithFormat:@"%dx%d", width, height].UTF8String;
     
-    // TODO: Add preference toggle 
     NSError *sessionError = nil;
     AVAudioSessionCategory category;
     AVAudioSessionCategoryOptions options;
-    if([getPreference(@"silence_other_audio") boolValue]) {
+    if([getPreference(@"silence_with_switch") boolValue]) {
         category = AVAudioSessionCategorySoloAmbient;
     } else {
         category = AVAudioSessionCategoryPlayback;
     }
-    
-    if([getPreference(@"silence_with_switch") boolValue]) {
+    if([getPreference(@"silence_other_audio") boolValue]) {
         options = AVAudioSessionCategoryOptionMixWithOthers;
     } else {
         options = 0;