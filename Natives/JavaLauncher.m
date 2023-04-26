bb588ca7e6dd49fbcafacd88ef66cf21f56505b5
Duy Tran Khanh <khanhduytran0@users.noreply.github.com>
2023-04-26 11:09:41 +0700

    Add missing LIBGL env vars

--- Natives/JavaLauncher.m
+++ Natives/JavaLauncher.m
@@ -28,8 +28,14 @@ void init_loadCustomEnv() {
     // Silent Caciocavallo NPE error in locating Android-only lib
     setenv("LD_LIBRARY_PATH", "", 1);
 
+    // Ignore mipmap for performance(?)
+    setenv("LIBGL_MIPMAP", "3", 1);
+
     // Disable overloaded functions hack for Minecraft 1.17+
     setenv("LIBGL_NOINTOVLHACK", "1", 1);
+
+    // Fix white color on banner and sheep, since GL4ES 1.1.5
+    setenv("LIBGL_NORMALIZE", "1", 1);
 
     // Override OpenGL version to 4.1 for Zink
     setenv("MESA_GL_VERSION_OVERRIDE", "4.1", 1);
